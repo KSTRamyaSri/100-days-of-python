775bfe9658e6d4937e703503cd60958d46f2c597
K S T Ramya Sri <kstramyasriswarupa@gmail.com>
2025-12-26 21:32:12 +0530

    Day 1 Python practice

---
+++ day1.py
@@ -0,0 +1,6 @@
+# DAY1 - PROJECT : BAND NAME GENERATOR
+
+print("BAND NAME GENERATION")
+city = input("Enter your city name : ")
+pet = input("Enter you pet name : ")
+print("your Band name is : "+city+pet)
