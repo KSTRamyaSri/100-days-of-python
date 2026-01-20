f542bd56e511da67f5e8439d172e962643ddb6f4
K S T Ramya Sri <kstramyasriswarupa@gmail.com>
2026-01-20 22:17:15 +0530

    day 6 of python

---
+++ day6.py
@@ -0,0 +1,24 @@
+# Day 6: Hurdles Race Reborg
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def step():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()        
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal() :
+    if wall_in_front():
+        step()
+    else :
+        move()
+        
